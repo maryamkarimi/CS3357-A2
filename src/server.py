b02f4d5b679d7987afd51cf662caa5394d19c765
Maryam Karimi <mkarimif@uwo.ca>
2020-10-12 19:43:37 -0400

    re-organized and refactored some of the code for better readability
    made the HttpStatus class an enum

--- src/server.py
+++ src/server.py
@@ -1,7 +1,8 @@
 import socket
+from enum import Enum
 
 # Address for the server
-TCP_IP = 'localhost'
+TCP_IP = '127.0.0.1'
 
 # Port for the server
 TCP_PORT = 8090
@@ -9,16 +10,21 @@ TCP_PORT = 8090
 BLANK_LINE = '\r\n'
 
 
-class HTTPStatus:
+class HTTPStatus(Enum):
     # success
-    OK = 200, 'OK', 'Request fulfilled, document follows'
+    OK = (200, 'OK', 'Request fulfilled, document follows')
 
     # client error
-    NOT_FOUND = 404, 'Not Found', 'Nothing matches the given URI'
+    NOT_FOUND = (404, 'Not Found', 'Nothing matches the given URI')
 
     # server errors
-    NOT_IMPLEMENTED = 501, 'Method Not Implemented', 'Server does not support this operation'
-    HTTP_VERSION_NOT_SUPPORTED = 505, 'Version Not Supported', 'This web server only supports HTTP/1.1.'
+    NOT_IMPLEMENTED = (501, 'Method Not Implemented', 'Server does not support this operation')
+    HTTP_VERSION_NOT_SUPPORTED = (505, 'Version Not Supported', 'This web server only supports HTTP/1.1.')
+
+    def __init__(self, status_code, status_description, status_message):
+        self.status_code = status_code
+        self.status_description = status_description
+        self.status_message = status_message
 
 
 class HTTPServer:
@@ -36,7 +42,7 @@ class HTTPServer:
 
         while True:
             connection_socket, addr = server_socket.accept()
-            print('Request recieved from ', addr)
+            print('Request received from ', addr)
 
             # Read data
             request = connection_socket.recv(1024).decode()
@@ -47,29 +53,26 @@ class HTTPServer:
 
                 # Send response back to the client
                 connection_socket.send(response)
-            except IndexError:
+            except ValueError:
                 connection_socket.close()
 
     # GET command
-    @staticmethod
-    def handle_get(args):
+    def handle_get(self, args):
 
+        method, file_name, http_version = args
         content_type = 'text-html'
         response_headers = ''
+        response_body = ''
 
-        if args[0].upper() != 'GET':
-            response_line = "HTTP/1.1 %s %s" % (HTTPStatus.NOT_IMPLEMENTED[0], HTTPStatus.NOT_IMPLEMENTED[1])
-            response_body = HTTPStatus.NOT_IMPLEMENTED[2].encode()
+        if method.upper() != 'GET':
+            status = HTTPStatus.NOT_IMPLEMENTED
 
-        elif args[2].upper() != 'HTTP/1.1':
-            response_line = "HTTP/1.1 %s %s" % (
-                HTTPStatus.HTTP_VERSION_NOT_SUPPORTED[0], HTTPStatus.HTTP_VERSION_NOT_SUPPORTED[1])
-            response_body = HTTPStatus.HTTP_VERSION_NOT_SUPPORTED[2].encode()
+        elif http_version.upper() != 'HTTP/1.1':
+            status = HTTPStatus.HTTP_VERSION_NOT_SUPPORTED
 
         else:
             try:
                 # if the file name starts with a '/', add a '.' to it so it can be properly handled.
-                file_name = args[1]
                 if file_name.startswith('/'):
                     file_name = '.' + file_name
 
@@ -78,25 +81,33 @@ class HTTPServer:
 
                 response_body = f.read()
 
-                file_type = args[1].split('.')[1]
-                if file_type.upper() == 'JPG' or file_type.upper() == 'JPEG' or file_type.upper() == 'PNG' or file_type.upper() == 'GIF':
+                file_type = file_name.split('.')[1].upper()
+                if file_type == 'JPG' or file_type == 'JPEG' or file_type == 'PNG' or file_type == 'GIF':
                     content_type = file_type
 
-                # set headers
-                response_headers = "Content-Length: %s" % (f.tell())
-                response_line = "HTTP/1.1 %s %s" % (HTTPStatus.OK[0], HTTPStatus.OK[1])
+                # set headers and status
+                file_size = f.tell()
+                response_headers = "Content-Length: %s" % file_size
+                status = HTTPStatus.OK
 
                 # close the file
                 f.close()
 
             except IOError:
-                response_line = "HTTP/1.1 %s %s" % (HTTPStatus.NOT_FOUND[0], HTTPStatus.NOT_FOUND[1])
-                response_body = HTTPStatus.NOT_FOUND[2].encode()
+                status = HTTPStatus.NOT_FOUND
 
-        response_headers = 'Content-type: %s\r\n%s' % (content_type, response_headers)
+        response_line = self.get_response_line(status.status_code, status.status_description)
+
+        if status != HTTPStatus.OK:
+            response_body = status.status_message.encode()
+
+        response_headers = 'Content-type: %s%s%s' % (content_type, BLANK_LINE, response_headers)
         response_headers = "%s%s%s%s%s" % (response_line, BLANK_LINE, response_headers, BLANK_LINE, BLANK_LINE)
-        response_headers = response_headers.encode()
-        return response_headers + response_body
+        return response_headers.encode() + response_body
+
+    @staticmethod
+    def get_response_line(response_code, response_description):
+        return "HTTP/1.1 %s %s" % (response_code, response_description)
 
 
 if __name__ == '__main__':
